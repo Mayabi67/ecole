53ba71d70b1e383e991bf4a3dab344dbdfb09cfb
Mayabi67 <mathiasmayabi@students.uonbi.ac.ke>
2024-06-16 23:35:16 +0300

    first commit

--- ecole_project/urls.py
+++ ecole_project/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path ("sim/", include('sim.urls')),
+    path("sim/", include("django.contrib.auth.urls")),
 ]
